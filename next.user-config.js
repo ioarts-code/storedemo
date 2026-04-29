9bf6959d03b2809f4f6f93609747e75b2e7b094f
Anders Altmann <brevduva999@proton.me>
2026-04-29 23:45:42 +0200

    Add files via upload

---
+++ next.user-config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your custom Next.js configuration here
+}
+
+module.exports = nextConfig
